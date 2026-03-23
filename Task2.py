a5aba795a6d0753067249bd1bbb2e6aff79049d4
v.varlygin <varlygin03@gmail.com>
2026-03-23 20:37:35 +0300

    Task2

---
+++ Task2.py
@@ -0,0 +1,14 @@
+def sort_stack(stack):
+    
+    temp_stack = []
+    while stack:
+        temp = stack.pop()
+        while temp_stack and temp_stack[-1] > temp:
+            stack.append(temp_stack.pop())
+        
+        temp_stack.append(tmp)
+    
+    while temp_stack:
+        stack.append(temp_stack.pop())
+    
+    return stack
